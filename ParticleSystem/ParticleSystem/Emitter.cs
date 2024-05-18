3bcdf1cc7d6a052a990d681445293fc05a1e8446
DinoBombino <137479606+DinoBombino@users.noreply.github.com>
2024-05-18 22:55:41 +0800

    Снег...

---
+++ ParticleSystem/ParticleSystem/Emitter.cs
@@ -0,0 +1,116 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Drawing;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace ParticleSystem
+{
+    public class Emitter
+    {
+        List<Particle> particles = new List<Particle>();
+        public List<IImpactPoint> impactPoints = new List<IImpactPoint>();
+
+        public int MousePositionX;
+        public int MousePositionY;
+        public float GravitationX = 0;
+        public float GravitationY = 1; // пусть гравитация будет силой один пиксель за такт, нам хватит
+
+        public int ParticlesCount = 500;
+
+        public int X; // координата X центра эмиттера, будем ее использовать вместо MousePositionX
+        public int Y; // соответствующая координата Y 
+        public int Direction = 0; // вектор направления в градусах куда сыпет эмиттер
+        public int Spreading = 360; // разброс частиц относительно Direction
+        public int SpeedMin = 2; // начальная минимальная скорость движения частицы
+        public int SpeedMax = 12; // начальная максимальная скорость движения частицы
+        public int RadiusMin = 2; // минимальный радиус частицы
+        public int RadiusMax = 10; // максимальный радиус частицы
+        public int LifeMin = 20; // минимальное время жизни частицы
+        public int LifeMax = 100; // максимальное время жизни частицы
+        public int ParticlesPerTick = 10;
+
+        public Color ColorFrom = Color.White; // начальный цвет частицы
+        public Color ColorTo = Color.FromArgb(0, Color.Black); // конечный цвет частиц
+
+        public virtual Particle CreateParticle()
+        {
+            var particle = new ParticleColorful();
+            particle.FromColor = ColorFrom;
+            particle.ToColor = ColorTo;
+
+            return particle;
+        }
+        public void UpdateState()
+        {
+            int particlesToCreate = ParticlesPerTick;
+
+            foreach (var particle in particles)
+            {
+                if (particle.Life <= 0)// если здоровье кончилось
+                {
+                    if (particlesToCreate > 0)
+                    {
+                        /* у нас как сброс частицы равносилен созданию частицы */
+                        particlesToCreate -= 1; // поэтому уменьшаем счётчик созданных частиц на 1
+                        ResetParticle(particle);
+                    }
+                }
+                else
+                {
+                    particle.X += particle.SpeedX;
+                    particle.Y += particle.SpeedY;
+
+                    particle.Life -= 1;
+
+                    particle.SpeedX += GravitationX;
+                    particle.SpeedY += GravitationY;
+                }
+            }
+            while (particlesToCreate >= 1)
+            {
+                for (var i = 0; i < 15; ++i)
+                {
+                    particlesToCreate -= 1;
+                    var particle = CreateParticle();
+                    ResetParticle(particle);
+                    particles.Add(particle);
+                }
+            }
+            
+        }
+
+        // добавил новый метод, виртуальным, чтобы переопределять можно было
+        public virtual void ResetParticle(Particle particle)
+        {
+            particle.Life = Particle.rand.Next(LifeMin, LifeMax);
+
+            particle.X = X;
+            particle.Y = Y;
+
+            var direction = Direction + (double)Particle.rand.Next(Spreading) - Spreading / 2;
+
+            var speed = Particle.rand.Next(SpeedMin, SpeedMax);
+
+            particle.SpeedX = (float)(Math.Cos(direction / 180 * Math.PI) * speed);
+            particle.SpeedY = -(float)(Math.Sin(direction / 180 * Math.PI) * speed);
+
+            particle.Radius = Particle.rand.Next(RadiusMin, RadiusMax);
+        }
+
+        public void Render(Graphics g)
+        {
+            foreach (var particle in particles)
+            {
+                particle.Draw(g);
+            }
+
+            // рисую точки притяжения красными кружочками
+            foreach (var point in impactPoints)
+            {
+                point.Render(g); // это добавили
+            }
+        }
+    }
+}
---
+++ ParticleSystem/ParticleSystem/Form1.cs
@@ -0,0 +1,56 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+
+namespace ParticleSystem
+{
+    public partial class Form1 : Form
+    {
+        List<Particle> particles = new List<Particle>();
+        //Emitter emitter = new Emitter();
+        Emitter emitter; // добавим поле для эмиттера
+
+        // добавляем переменные для хранения положения мыши
+        private int MousePositionX = 0;
+        private int MousePositionY = 0;
+        public Form1()
+        {
+            InitializeComponent();
+
+            picDisplay.Image = new Bitmap(picDisplay.Width, picDisplay.Height);
+
+            emitter = new TopEmitter  /*Для снега!*/
+            {
+                Width = picDisplay.Width,
+                GravitationY = 0.25f
+            };/**/
+        }
+
+        //int counter = 0; // добавлю счетчик чтобы считать вызовы функции
+        private void timer1_Tick(object sender, EventArgs e)
+        {
+            emitter.UpdateState();
+
+            using (var g = Graphics.FromImage(picDisplay.Image))
+            {
+                g.Clear(Color.Black); // А ЕЩЕ ЧЕРНЫЙ ФОН СДЕЛАЮ
+                emitter.Render(g);
+            }
+
+            picDisplay.Invalidate();
+        }
+
+        private void picDisplay_MouseMove(object sender, MouseEventArgs e)
+        {
+            // в обработчике заносим положение мыши в переменные для хранения положения мыши
+            emitter.MousePositionX = e.X;
+            emitter.MousePositionY = e.Y;
+        }
+    }
+}
